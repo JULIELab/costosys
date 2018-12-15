258ff1330a50d42444ffb9efa464e395d001e24f
khituras <chew@gmx.net>
2018-12-15 09:20:50 +0100

    Fixing code signing before-deploy.sh script

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	openssl aes-256-cbc -K $encrypted_d47fb4320ded_key -iv $encrypted_d47fb4320ded_iv -in codesigning.asc.enc -out ~\/.gnupg/codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_d47fb4320ded_key -iv $encrypted_d47fb4320ded_iv -in cd/codesigning.asc.enc -out ~\/.gnupg/codesigning.asc -d
 	gpg --fast-import cd/signingkey.asc
 fi
 
