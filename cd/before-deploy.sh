e1938abe9e6001f46834a775fe386472339d12a3
khituras <chew@gmx.net>
2018-12-25 10:54:52 +0100

    PGP key couldn't be decrypted by travis, encrypting again

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	openssl aes-256-cbc -K $encrypted_d47fb4320ded_key -iv $encrypted_d47fb4320ded_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_be3f5c67f3aa_key -iv $encrypted_be3f5c67f3aa_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
 	gpg --fast-import cd/codesigning.asc
 fi
 
