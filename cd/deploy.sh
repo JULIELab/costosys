f17edceb982486a9c1f87535fbd527da1a2047bf
khituras <chew@gmx.net>
2019-06-12 16:05:14 +0200

    Setting the -B(atch) option to the `mvn deploy` command to save log lines.
    Because the travis build failed due to the large log file.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	echo "Executing deploy"
-    mvn deploy -P sonatype-nexus-deployment --settings cd/mvnsettings.xml
+    mvn deploy -B -P sonatype-nexus-deployment --settings cd/mvnsettings.xml
 else
 	echo "Deploy not executed"
 fi
